1cb31713e1aeacca7381aa120cb3e2d6b0df88eb
¨RodrigoCSoares¨ <soares.c.rodrigo@gmail.com>
2019-07-02 11:42:29 -0300

    First query added to the schema

--- src/index.js
+++ src/index.js
@@ -2,13 +2,35 @@ const { GraphQLServer } = require('graphql-yoga');
 const typeDefs = 
     'type Query {\
         info: String!\
+        feed: [Link!]!\
+    }\
+    \
+    type Link {\
+        id: ID!\
+        description: String!\
+        url: String!\
     }'
 ;
+
+let links = [{
+    id: 'link-0',
+    url: 'www.url.com',
+    description: 'description'
+}];
+
 const resolvers = {
     Query: {
         info: () => 'This is the API of a Hackernews Clone',
+        feed: () => links,
+    },
+
+    Link: {
+        id: (parent) => parent.id,
+        description: (parent) => parent.description,
+        url: (parent) => parent.url,
     }
 };
+
 const server = new GraphQLServer({
     typeDefs,
     resolvers,
