14951468aa6857259984fb6bde62a476854c08ad
¨RodrigoCSoares¨ <soares.c.rodrigo@gmail.com>
2019-07-16 10:23:56 -0300

    Added filtering, pagination and sorting

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -2,8 +2,29 @@ function info(parent, args, context, info) {
     return 'This is the API of a Hackernews Clone';
 }
 
-function feed(parent, args, context, info) {
-    return context.prisma.links();
+async function feed(parent, args, context, info) {
+    const where = args.filter ? {
+        OR: [
+            { description_contains: args.filter },
+            { url_contains: args.filter },
+        ],
+    } : {};
+
+    const links = await context.prisma.links({ 
+        where,
+        skip: args.skip,
+        first: args.first,
+        orderBy: args.orderBy
+     });
+
+     const count = await context.prisma
+        .linksConnection({
+            where,
+        })
+        .aggregate()
+        .count();
+
+    return { links, count };
 }
 
 function link(parent, args, context, info) {
